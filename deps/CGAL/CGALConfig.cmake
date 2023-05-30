c6754d89593819a4085fdc3cdc7cae2903ce76d5
Hossein Baktash <hbaktash@hosseins-mbp.corp.adobe.com>
2023-05-30 12:05:57 -0400

    remove CGAL stuff

--- deps/CGAL/CGALConfig.cmake
+++
@@ -1,5 +0,0 @@
-# 
-# This file points to the CGALConfig.cmake for header-only CGAL.
-#
-
-include(/Users/hbaktash/Desktop/code/gc-polyscope-project-template/deps/CGAL/lib/cmake/CGAL/CGALConfig.cmake)
