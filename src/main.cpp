d108e87feea4bc3fbc66031be1138a4b855eb680
Hossein Baktash <hbaktash@hosseins-mbp.corp.adobe.com>
2023-06-01 17:54:00 -0400

    skewed 4-gon added

--- src/main.cpp
+++ src/main.cpp
@@ -27,14 +27,24 @@ std::unique_ptr<ManifoldSurfaceMesh> mesh_ptr;
 std::unique_ptr<VertexPositionGeometry> geometry_ptr;
 ManifoldSurfaceMesh* mesh;
 VertexPositionGeometry* geometry;
-
+Vector3 G; // center of Mass
 
 ForwardSolver forwardSolver;
 
 // Polyscope visualization handle, to quickly add data to the surface
-polyscope::SurfaceMesh *psInputMesh, *dummy_psMesh1;
+polyscope::SurfaceMesh *psInputMesh, *dummy_psMesh1, *dummy_psMesh2, *dummy_psMesh3;
 
-float pt_cloud_radi_scale = 1.;
+float pt_cloud_radi_scale = 0.1,
+      curve_radi_scale = 0.1,
+      G_scale = 1.,
+      G_angle = 0.;
+
+
+// example choice
+std::vector<std::string> all_polygon_items = {std::string("cube"), std::string("skewed 4-gon"), std::string("rndbs 6-gon 1")};
+std::string all_polygons_current_item = "cube";
+static const char* all_polygons_current_item_c_str = "cube";
+
 
 void visualize_boundary_curves(){
   std::vector<std::vector<size_t>> dummy_face{{1,1,1}};
@@ -58,6 +68,7 @@ void visualize_vertex_probabilities(){
     std::vector<Vector3> positions = {forwardSolver.hullGeometry->inputVertexPositions[v]};
     polyscope::PointCloud* psCloud = polyscope::registerPointCloud("v" + std::to_string(v.getIndex()), positions);
     // set some options
+    psCloud->setPointColor({forwardSolver.vertex_probabilities[v], 1., 1.});
     psCloud->setPointRadius(forwardSolver.vertex_probabilities[v] * pt_cloud_radi_scale);
     psCloud->setPointRenderMode(polyscope::PointRenderMode::Sphere);
   }
@@ -68,25 +79,50 @@ void visualize_edge_probabilities(){
   forwardSolver.build_next_edge_tracer();
   forwardSolver.compute_initial_edge_probabilities();
   forwardSolver.compute_final_edge_probabilities();
+
+  // initial probs
+  std::vector<std::vector<size_t>> dummy_face{{1,1,1}};
+  dummy_psMesh2 = polyscope::registerSurfaceMesh(
+      "initial prob",
+      geometry->inputVertexPositions, dummy_face);
+  
+  dummy_psMesh3 = polyscope::registerSurfaceMesh(
+      "final prob",
+      geometry->inputVertexPositions, dummy_face);
+  // auto positions = forwardSolver.hullGeometry->inputVertexPositions.toVector();
+  // std::vector<std::array<size_t, 2>> edgeInds;
+  for (Edge e: forwardSolver.hullMesh->edges()){
+    if (e.isBoundary()){
+      std::vector<std::array<size_t, 2>> edgeInds;
+      std::vector<Vector3> positions;
+      edgeInds.push_back({0, 1});
+      Vector3 p1 = forwardSolver.hullGeometry->inputVertexPositions[e.firstVertex()],
+              p2 = forwardSolver.hullGeometry->inputVertexPositions[e.secondVertex()];
+      positions.push_back(p1); positions.push_back(p2);
+      // initial prob visualize
+      polyscope::SurfaceGraphQuantity* psSegment =  dummy_psMesh2->addSurfaceGraphQuantity("initial prob e" + std::to_string(e.getIndex()), positions, edgeInds);
+      psSegment->setRadius(forwardSolver.initial_edge_probabilities[e]*curve_radi_scale);
+      psSegment->setColor({1., forwardSolver.initial_edge_probabilities[e], 1.});
+      psSegment->setEnabled(true);
+      // final prob visualize
+      polyscope::SurfaceGraphQuantity* psSegment2 =  dummy_psMesh3->addSurfaceGraphQuantity("final prob e" + std::to_string(e.getIndex()), positions, edgeInds);
+      psSegment2->setRadius(forwardSolver.final_edge_probabilities[e]*curve_radi_scale);
+      psSegment2->setColor({1., 1., forwardSolver.final_edge_probabilities[e]});
+      psSegment2->setEnabled(true);
+    }
+  }
+  
 }
 
-// A user-defined callback, for creating control panels (etc)
-// Use ImGUI commands to build whatever you want here, see
-// https://github.com/ocornut/imgui/blob/master/imgui.h
-void myCallback() {
-  if (ImGui::Button("build boundary curves")) {
-    visualize_boundary_curves();
-  }
-  if (ImGui::Button("visualize vertex probabilities")) {
-    visualize_vertex_probabilities();
-  }
-  if (ImGui::SliderFloat("radi scale", &pt_cloud_radi_scale, 0., 1.)) visualize_vertex_probabilities();
-
-  if (ImGui::Button("visualize edge probabilities")) {
-    visualize_edge_probabilities();
-  }
+// visualize center of mass
+void draw_G() {
+  std::vector<Vector3> G_position = {forwardSolver.G};
+  polyscope::PointCloud* psG = polyscope::registerPointCloud("Center of Mass", G_position);
+  // set some options
+  psG->setPointColor({1., 1., 1.});
+  psG->setPointRadius(pt_cloud_radi_scale/2.);
+  psG->setPointRenderMode(polyscope::PointRenderMode::Sphere);
 }
-
 
 // generate simple examples
 void generate_polygon_example(std::string poly_str){
@@ -99,6 +135,23 @@ void generate_polygon_example(std::string poly_str){
                               {0, 2, 3}};
       positions = {{-1, -1, 0}, {1, -1, 0}, {1, 1, 0}, {-1, 1, 0}};
     }
+    else if (std::strcmp(poly_str.c_str(), "skewed 4-gon") == 0){
+      n = 4;
+      greedy_triangulation = {{0, 1, 2},
+                              {0, 2, 3}};
+      positions = {{-1*3., -1*3., 0}, {1*1.5, -1*1.5, 0}, {1*2, 1*2, 0}, {-1, 1, 0}};
+    }
+    else if (std::strcmp(poly_str.c_str(), "rndbs 6-gon 1") == 0){
+      n = 6;
+      greedy_triangulation = {{0, 1, 2},
+                              {0, 2, 3},
+                              {0, 3, 4},
+                              {0, 4, 5}};
+      for (int i = 0; i++ ; i < 6) {
+        double tmp_angle = (double)i*2*PI/6.;
+        positions.push_back({cos(tmp_angle), sin(tmp_angle), 0.});
+      }
+    }
     else {
       throw std::runtime_error("no valid string provided\n");
     }
@@ -110,7 +163,70 @@ void generate_polygon_example(std::string poly_str){
         // Use the low-level indexers here since we're constructing
         geometry->inputVertexPositions[v] = positions[v.getIndex()];
     }
+    printf("$$$$$$$$$$ here1 $$$$$$$$$$$$\n");
 }
+
+void update_solver(){
+  forwardSolver = ForwardSolver(mesh, geometry, G);
+  //assuming convex input here
+  forwardSolver.hullMesh = new ManifoldSurfaceMesh(mesh->getFaceVertexList()); //mesh->copy().release();
+  forwardSolver.hullGeometry = new VertexPositionGeometry(*forwardSolver.hullMesh); // geometry->copy().release();
+  for (Vertex v: forwardSolver.hullMesh->vertices()){
+    forwardSolver.hullGeometry->inputVertexPositions[v] = geometry->inputVertexPositions[v.getIndex()];
+  }
+  forwardSolver.compute_vertex_probabilities();
+  // Register the mesh with polyscope
+  psInputMesh = polyscope::registerSurfaceMesh(
+      "input mesh",
+      geometry->inputVertexPositions, mesh->getFaceVertexList(),
+      polyscopePermutations(*mesh));
+  draw_G();
+}
+
+// A user-defined callback, for creating control panels (etc)
+// Use ImGUI commands to build whatever you want here, see
+// https://github.com/ocornut/imgui/blob/master/imgui.h
+void myCallback() {
+  if (ImGui::BeginCombo("##combo1", all_polygons_current_item.c_str())){ // The second parameter is the label previewed before opening the combo.
+        for (std::string tmp_str: all_polygon_items){ // This enables not having to have a const char* arr[]. Or maybe I'm just a noob.
+            bool is_selected = (all_polygons_current_item == tmp_str.c_str()); // You can store your selection however you want, outside or inside your objects
+            if (ImGui::Selectable(tmp_str.c_str(), is_selected)){ // selected smth
+                all_polygons_current_item = tmp_str;
+                generate_polygon_example(all_polygons_current_item);
+                update_solver();
+            }
+            if (is_selected)
+                ImGui::SetItemDefaultFocus();   // You may set the initial focus when opening the combo (scrolling + for keyboard navigation support)
+        }
+        ImGui::EndCombo();
+    }
+
+  if (ImGui::Button("build boundary curves")) {
+    visualize_boundary_curves();
+  }
+  if (ImGui::Button("visualize vertex probabilities")) {
+    visualize_vertex_probabilities();
+  }
+  if (ImGui::SliderFloat("vertex radi scale", &pt_cloud_radi_scale, 0., 1.)) {
+    draw_G();
+    visualize_vertex_probabilities();
+  }
+  if (ImGui::SliderFloat("edge radi scale", &curve_radi_scale, 0., 1.)) visualize_edge_probabilities();
+
+  if (ImGui::Button("visualize edge probabilities")) {
+    visualize_edge_probabilities();
+  }
+
+  if (ImGui::SliderFloat("G radi scale", &G_scale, -3., 3.)||
+      ImGui::SliderFloat("G angle", &G_angle, 0., 2*PI)) {
+    G = {cos(G_angle)*G_scale, sin(G_angle) * G_scale, 0};
+    forwardSolver.G = G;
+    draw_G();
+    visualize_edge_probabilities();
+  }
+
+}
+
 
 int main(int argc, char **argv) {
 
@@ -137,33 +253,20 @@ int main(int argc, char **argv) {
   // }
 
   // build mesh
-  generate_polygon_example("cube");
+  generate_polygon_example(all_polygons_current_item);
+  update_solver();
   // build the solver
-  Vector3 G = {0.99,0,0};
-  forwardSolver = ForwardSolver(mesh, geometry, G);
-  //assuming convex input here
-  forwardSolver.hullMesh = new ManifoldSurfaceMesh(mesh->getFaceVertexList()); //mesh->copy().release();
-  forwardSolver.hullGeometry = new VertexPositionGeometry(*forwardSolver.hullMesh); // geometry->copy().release();
-  for (Vertex v: forwardSolver.hullMesh->vertices()){
-    forwardSolver.hullGeometry->inputVertexPositions[v] = geometry->inputVertexPositions[v.getIndex()];
-  }
-  forwardSolver.compute_vertex_probabilities();
+  G = {0.99,0,0};
   
   // Load mesh
   // std::tie(mesh, geometry) = readManifoldSurfaceMesh(args::get(inputFilename));
 
   // Initialize polyscope
   polyscope::init();
-  // Register the mesh with polyscope
-  psInputMesh = polyscope::registerSurfaceMesh(
-      "input mesh",
-      geometry->inputVertexPositions, mesh->getFaceVertexList(),
-      polyscopePermutations(*mesh));
-
+  
   // Set the callback function
   polyscope::state::userCallback = myCallback;
-
-  
+  polyscope::view::style = polyscope::view::NavigateStyle::Planar;
   // Set vertex tangent spaces
   
   // Give control to the polyscope gui
