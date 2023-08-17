aa6ced30fc6eb56f340b80c651bc468d8a737b28
Hossein Baktash <hbaktash@adobe.com>
2023-08-17 18:46:57 -0400

    sudoEdge to sudoEdge flow..

--- src/arc_algebra.cpp
+++ src/arc_algebra.cpp
@@ -1,2 +1,7 @@
 #include "arc_algebra.h"
 
+
+Vector3 intersect_arc_ray_with_arc(Vector3 R1, Vector3 R2, Vector3 A, Vector3 B){
+    assert(R1.norm() == 1. && R2.norm() == 1. && A.norm() == 1. && B.norm() == 1.);
+    Vector3 ray_plane_normal = cross(R1, R2);
+}
