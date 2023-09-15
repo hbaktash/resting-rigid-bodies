274f14a2b979c042ce068b3c70db4176f5dcd130
hbaktash <hbaktash@andrew.cmu.edu>
2023-09-15 17:59:08 -0400

    improving boundary builder; recursive

--- src/boundary_tools.cpp
+++ src/boundary_tools.cpp
@@ -40,7 +40,7 @@ BoundaryBuilder::BoundaryBuilder(Forward3DSolver *forward_solver_){
 }
 
 
- void BoundaryBuilder::build_boundary_normals(){
+void BoundaryBuilder::build_boundary_normals(){
     vertex_boundary_normal = VertexData<BoundaryNormal*>(*mesh, nullptr);
     edge_boundary_normals = EdgeData<std::vector<BoundaryNormal*>>(*mesh);
 
@@ -53,7 +53,7 @@ BoundaryBuilder::BoundaryBuilder(Forward3DSolver *forward_solver_){
     for (Edge e: mesh->edges()){
         if (forward_solver->edge_is_singular[e]){
             Face f1 = e.halfedge().face(),
-                 f2 = e.halfedge().twin().face();
+                    f2 = e.halfedge().twin().face();
             if (forward_solver->face_last_face[f1] != forward_solver->face_last_face[f2]){ 
                 // proved: at least one singular edge like this must exist
                 // TODO: assert that stable normal falls inside the edge arc 
@@ -70,31 +70,35 @@ BoundaryBuilder::BoundaryBuilder(Forward3DSolver *forward_solver_){
         assert(edge_boundary_normals[e].size() == 1);
         BoundaryNormal *bnd_normal = edge_boundary_normals[e].front();
         for (Vertex v: e.adjacentVertices()){
-            if (forward_solver->vertex_is_stabilizable[v]){
-                Vector3 stable_vertex_normal = forward_solver->vertex_stable_normal[v];
-                BoundaryNormal *new_boundary_normal = new BoundaryNormal(stable_vertex_normal);
-                new_boundary_normal->add_neighbor(bnd_normal);
-                bnd_normal->add_neighbor(new_boundary_normal);
-            }
-            else {
-                for (Edge neigh_e: v.adjacentEdges()){
-                    if (neigh_e != e){
-                        flow_back_boundary_on_edge(e, bnd_normal, neigh_e, v);
-                    }
+            for (Edge neigh_e: v.adjacentEdges()){
+                if (neigh_e != e){
+                    flow_back_boundary_on_edge(e, bnd_normal, neigh_e, v);
                 }
             }
         }
     }
-    
- }
+}
 
 
+// recursively follow the boundary curve to a source
 void BoundaryBuilder::flow_back_boundary_on_edge(Edge dest_e, BoundaryNormal* bnd_normal, Edge src_e, Vertex common_vertex){
     // bnd_normal has to be in boundary normals of dest_e; won't assert tho for better performance
-    
+    Vertex v = common_vertex; // given as argument for better performance
+    if (forward_solver->vertex_is_stabilizable[v]){ // we are at a source
+        BoundaryNormal *curr_vertex_boundary_normal = vertex_boundary_normal[v];
+        if (vertex_boundary_normal[v] == nullptr){ // first time arriving at this stable vertex; create the boundary normal
+            Vector3 stable_vertex_normal = forward_solver->vertex_stable_normal[v];
+            curr_vertex_boundary_normal = new BoundaryNormal(stable_vertex_normal);
+            vertex_boundary_normal[v] = curr_vertex_boundary_normal;
+        }
+        curr_vertex_boundary_normal->add_neighbor(bnd_normal);
+        bnd_normal->add_neighbor(curr_vertex_boundary_normal);
+        return;
+    }
+    // vertex is not a source
+    // TODO speed up with precomputed edge source detection?
     if (forward_solver->edge_is_singular[src_e])
         return; // then src_e cannot really be a source 
-    Vertex v = common_vertex; // given as argument for better performance
     Face f1 = src_e.halfedge().face(),
          f2 = src_e.halfedge().twin().face();
          
